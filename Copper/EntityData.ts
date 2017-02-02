d06b458f65a48844a5c358c286ec265cd48d9ab1
Ben Cracknell <ben.cracknell@dockyardgames.com>
2017-02-02 12:20:12 -0800

    Initial Commit

---
+++ Copper/EntityData.ts
@@ -0,0 +1,17 @@
+
+
+/** Retreives all entities and their data belonging to the player who calls */
+handlers.getPlayerEntities = function(args: IGetPlayerEntitiesRequest, context): IGetPlayerEntitiesResult{
+    log.info("Getting entity data for " + currentPlayerId);
+    return null;
+}
+
+interface IGetPlayerEntitiesRequest
+{
+
+}
+
+interface IGetPlayerEntitiesResult
+{
+
+}
