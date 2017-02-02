4812896cf5e27b5ba82b8a628a5f54ce4daf40af
Ben Cracknell <ben.cracknell@dockyardgames.com>
2017-02-02 12:26:02 -0800

    Test Change

--- Copper/EntityData.ts
+++ Copper/EntityData.ts
@@ -3,6 +3,7 @@
 /** Retreives all entities and their data belonging to the player who calls */
 handlers.getPlayerEntities = function(args: IGetPlayerEntitiesRequest, context): IGetPlayerEntitiesResult{
     log.info("Getting entity data for " + currentPlayerId);
+    log.info("test change!");
     return null;
 }
 
